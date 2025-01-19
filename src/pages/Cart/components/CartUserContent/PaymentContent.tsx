d5e4ae35510abdfff0b8bcb43e575b16ff781a46
Thiago Leite <thiagofmleite@gmail.com>
2025-01-19 18:16:30 -0300

    Add payment options to PaymentContent; implement radio buttons for credit card, debit card, and cash

--- src/pages/Cart/components/CartUserContent/PaymentContent.tsx
+++ src/pages/Cart/components/CartUserContent/PaymentContent.tsx
@@ -1,3 +1,77 @@
+import { Bank, CreditCard, CurrencyDollar, Money } from "@phosphor-icons/react";
+import {
+  CartUserFormContainer,
+  CartUserFormHeader,
+  RadioButton,
+  RadioContainer,
+  RadioLabel,
+  RadioWrapper,
+} from "./styles";
+
 export function PaymentContent() {
-  return <div>Payment Content</div>;
+  return (
+    <CartUserFormContainer>
+      <CartUserFormHeader>
+        <div className="header-icon-payment">
+          <CurrencyDollar size={30} />
+        </div>
+        <div className="header-text">
+          <h3>Pagamento</h3>
+          <p>
+            O pagamento é feito na entrega. Escolha a forma que deseja pagar
+          </p>
+        </div>
+      </CartUserFormHeader>
+
+      <RadioContainer>
+        <RadioWrapper>
+          <RadioButton
+            type="radio"
+            name="payment"
+            id="credit-card"
+            value="credit-card"
+            required
+          />
+          <RadioLabel htmlFor="credit-card">
+            <span className="icon">
+              <CreditCard size={30} />
+            </span>
+            <span className="text">Cartão de Crédito</span>
+          </RadioLabel>
+        </RadioWrapper>
+        <RadioWrapper>
+          <RadioButton
+            type="radio"
+            name="payment"
+            id="debit-card"
+            value="debit-card"
+            required
+          />
+          <RadioLabel htmlFor="debit-card">
+            {" "}
+            <span className="icon">
+              <Bank size={30} />
+            </span>
+            <span className="text">Cartão de Débito</span>
+          </RadioLabel>
+        </RadioWrapper>
+        <RadioWrapper>
+          <RadioButton
+            type="radio"
+            name="payment"
+            id="money"
+            value="money"
+            required
+          />
+          <RadioLabel htmlFor="money">
+            {" "}
+            <span className="icon">
+              <Money size={30} />
+            </span>
+            <span className="text">Dinheiro</span>
+          </RadioLabel>
+        </RadioWrapper>
+      </RadioContainer>
+    </CartUserFormContainer>
+  );
 }
