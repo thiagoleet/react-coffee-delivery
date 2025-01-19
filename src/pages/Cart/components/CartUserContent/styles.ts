b2a1958a98b4734e0230370ff1c10f04af2bccf2
Thiago Leite <thiagofmleite@gmail.com>
2025-01-19 18:16:26 -0300

    Add radio button styles and layout to CartUserContent; enhance header icon colors

--- src/pages/Cart/components/CartUserContent/styles.ts
+++ src/pages/Cart/components/CartUserContent/styles.ts
@@ -32,6 +32,10 @@ export const CartUserFormHeader = styled.header`
 
   .header-icon {
     color: ${({ theme }) => theme["yellow-dark"]};
+
+    &-payment {
+      color: ${({ theme }) => theme["purple-base"]};
+    }
   }
 
   .header-text {
@@ -90,5 +94,48 @@ export const InputField = styled.input`
 
   &.sm {
     flex: 0.25;
+  }
+`;
+export const RadioContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  justify-content: space-between;
+`;
+
+export const RadioWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const RadioButton = styled.input`
+  display: none;
+  &:checked + label {
+    border-color: ${({ theme }) => theme["purple-base"]};
+    background-color: ${({ theme }) => theme["purple-light"]};
+  }
+`;
+
+export const RadioLabel = styled.label`
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme["base-button"]};
+  background-color: ${({ theme }) => theme["base-button"]};
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  padding: 1rem;
+  text-transform: uppercase;
+  font-size: 0.9rem;
+
+  .text {
+  }
+
+  .icon {
+    color: ${({ theme }) => theme["purple-base"]};
   }
 `;
